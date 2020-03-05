4f537f622230b81d6f897cd5dd71eafc9765a364
millerjryan <50216489+millerjryan@users.noreply.github.com>
2020-03-05 11:24:02 -0500

    Update GenerateLogFile.ps1
    Missing top of the file

--- GenerateLogFile.ps1
+++ GenerateLogFile.ps1
@@ -1,3 +1,40 @@
+function Write-Log 
+{ 
+    [CmdletBinding()] 
+    Param 
+    ( 
+        [Parameter(Mandatory=$true, 
+                   ValueFromPipelineByPropertyName=$true)] 
+        [ValidateNotNullOrEmpty()] 
+        [Alias("LogContent")] 
+        [string]$Message, 
+ 
+        [Parameter(Mandatory=$false)] 
+        [Alias('LogPath')] 
+        [string]$Path='C:\Logs\PowerShellLog.log', 
+         
+        [Parameter(Mandatory=$false)] 
+        [ValidateSet("Error","Warn","Info")] 
+        [string]$Level="Info", 
+         
+        [Parameter(Mandatory=$false)] 
+        [switch]$NoClobber 
+    ) 
+ 
+    Begin 
+    { 
+        # Set VerbosePreference to Continue so that verbose messages are displayed. 
+        $VerbosePreference = 'Continue' 
+    } 
+    Process 
+    { 
+         
+        # If the file already exists and NoClobber was specified, do not write to the log. 
+        if ((Test-Path $Path) -AND $NoClobber) { 
+            Write-Error "Log file $Path already exists, and you specified NoClobber. Either delete the file or specify a different name." 
+            Return 
+            } 
+ 
         # If attempting to write to a log file in a folder/path that doesn't exist create the file including the path. 
         elseif (!(Test-Path $Path)) { 
             Write-Verbose "Creating $Path." 
