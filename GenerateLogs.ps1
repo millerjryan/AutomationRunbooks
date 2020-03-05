7446301cd2c4d033da8e2738d8b5c3fb6912f4ec
millerjryan <50216489+millerjryan@users.noreply.github.com>
2020-03-05 10:17:43 -0500

    Create GenerateLogs.ps1

---
+++ GenerateLogs.ps1
@@ -0,0 +1,42 @@
+        # If attempting to write to a log file in a folder/path that doesn't exist create the file including the path. 
+        elseif (!(Test-Path $Path)) { 
+            Write-Verbose "Creating $Path." 
+            $NewLogFile = New-Item $Path -Force -ItemType File 
+            } 
+ 
+        else { 
+            # Nothing to see here yet. 
+            } 
+ 
+        # Format Date for our Log File 
+        $FormattedDate = Get-Date -Format "yyyy-MM-dd HH:mm:ss" 
+ 
+        # Write message to error, warning, or verbose pipeline and specify $LevelText 
+        switch ($Level) { 
+            'Error' { 
+                Write-Error $Message 
+                $LevelText = 'ERROR:' 
+                } 
+            'Warn' { 
+                Write-Warning $Message 
+                $LevelText = 'WARNING:' 
+                } 
+            'Info' { 
+                Write-Verbose $Message 
+                $LevelText = 'INFO:' 
+                } 
+            } 
+         
+        # Write log entry to $Path 
+        "$FormattedDate $LevelText $Message" | Out-File -FilePath $Path -Append 
+    } 
+    End 
+    { 
+    } 
+}
+
+$GenGuid = [System.Guid]::NewGuid()
+$DateTime =  (Get-Date).ToString()
+
+Write-Log -Message "This is the new QUID: $GenGuid"
+Write-Log -Message "This is an warn on date :: $DateTime" -Level Warn
